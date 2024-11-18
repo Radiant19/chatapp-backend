a2942593d2b84b1c66da50661a7f677886894d56
Radiant19 <152776230+Radiant19@users.noreply.github.com>
2024-11-18 13:35:08 +0530

    changes

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
-import path from "path"
-import authRoutes from "./routes/authRoutes.js"
-import messageRoutes from "./routes/messageRoutes.js"
-import userRoutes from "./routes/userRoutes.js"
-import { app,server } from "./socket/socket.js";
+import path from "path";
+import authRoutes from "./routes/authRoutes.js";
+import messageRoutes from "./routes/messageRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import { app, server } from "./socket/socket.js";
 import Connection from "./db/connecttoMongoose.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const PORT = process.env.PORT || 5000;
 
@@ -17,20 +18,29 @@ dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/auth",authRoutes)
-app.use("/api/messages",messageRoutes)
-app.use("/api/users",userRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
-app.use(express.static(path.join(__dirname,"/frontend/dist")))
+app.use(
+  cors({
+    cors: {
+      origin: process.env.ORIGIN,
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+      credentials: true,
+    },
+  })
+);
 
-app.get("*",(req,res)=>{
-  res.sendFile(path.join(__dirname,"frontend","dist","index.html"))  
-} )
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 // app.get("/", (req, res) => {
 //   res.send("hello world!!!");
 // });
 
-
 server.listen(PORT, async () => {
-Connection(),
-console.log(`Server Running on port ${PORT}`)});
+  Connection(), console.log(`Server Running on port ${PORT}`);
+});
